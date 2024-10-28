0e7373f17eb7f3a83bc17c5f1a555ddf7215a6c1
Your Name <you@example.com>
2024-10-29 03:38:10 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Roboto } from 'next/font/google';
+import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
+
+const inter = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700']
+})
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +34,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased ${inter.className}`}
       >
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
